06323cf8a85933a81cc92853d522b0460eba8744
kevinandrieux <andrieux.kevin459@gmail.com>
2019-10-25 16:11:34 +0200

    sculptures admin fini sur les lien et le controller +galerie d'image

--- src/Controller/CarvingController.php
+++ src/Controller/CarvingController.php
@@ -2,93 +2,41 @@
 
 namespace App\Controller;
 
-use App\Entity\Carving;
-use App\Form\CarvingType;
 use App\Repository\CarvingRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-/**
- * @Route("/carving")
- */
+    /**
+     * @Route("/carving")
+    */
+
 class CarvingController extends AbstractController
 {
     /**
-     * @Route("/", name="carving_index", methods={"GET"})
+     * @Route("/", name="carvingwoods")
      */
-    public function index(CarvingRepository $carvingRepository): Response
+    public function woods(CarvingRepository $carvingRepository): Response
     {
-        return $this->render('Admin/carving/index.html.twig', [
-            'carvings' => $carvingRepository->findAll(),
+        
+        return $this->render('carving/carvingwood.html.twig', [
+            'controller_name' => 'CarvingController',
+            'bois' => $carvingRepository->findBy([
+                'support'=>'bois'
+        ])
         ]);
     }
-
-    /**
-     * @Route("/new", name="carving_new", methods={"GET","POST"})
-     */
-    public function new(Request $request): Response
-    {
-        $carving = new Carving();
-        $form = $this->createForm(CarvingType::class, $carving);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($carving);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('carving_index');
-        }
-
-        return $this->render('Admin/carving/new.html.twig', [
-            'carving' => $carving,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="carving_show", methods={"GET"})
-     */
-    public function show(Carving $carving): Response
-    {
-        return $this->render('Admin/carving/show.html.twig', [
-            'carving' => $carving,
-        ]);
-    }
-
-    /**
-     * @Route("/{id}/edit", name="carving_edit", methods={"GET","POST"})
-     */
-    public function edit(Request $request, Carving $carving): Response
-    {
-        $form = $this->createForm(CarvingType::class, $carving);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('carving_index');
-        }
-
-        return $this->render('Admin/carving/edit.html.twig', [
-            'carving' => $carving,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="carving_delete", methods={"DELETE"})
-     */
-    public function delete(Request $request, Carving $carving): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$carving->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($carving);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('carving_index');
-    }
-}
+//     /**
+//      * @Route("/stone", name="carvingwoods")
+//      */
+//     public function stone(CarvingRepository $carvingRepository): Response
+//     {
+        
+//         return $this->render('carving/carvingwood.html.twig', [
+//             'controller_name' => 'CarvingController',
+//             'bois' => $carvingRepository->findBy([
+//                 'support'=>'pierre'
+//         ])
+//         ]);
+//     }
+ }
